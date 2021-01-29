42dd640eca0b16e9300d86d69cf6f6485d1f1a3c
Aftab Alam <aftab.alam@aftabalam-ltm.internal.salesforce.com>
2021-01-28 18:53:08 -0800

    program name in submit.sh

--- submit.sh
+++ submit.sh
@@ -9,10 +9,10 @@ mkdir -p submit
 printf "Omkar Agashe,omagashe\nAftab Alam,aftaba\nAbduselam Shaltu,ashaltu" > submit/team.txt
 
 # train model
-python src/myprogram.py train --work_dir work
+python src/main.py train --work_dir work
 
 # make predictions on example data submit it in pred.txt
-python src/myprogram.py test --work_dir work --test_data example/input.txt --test_output submit/pred.txt
+python src/main.py test --work_dir work --test_data example/input.txt --test_output submit/pred.txt
 
 # submit docker file
 cp Dockerfile submit/Dockerfile
